b2192c2f8e476c39248380a12ec3b7c6ca8a7f1a
CatherineCAROU <carou.catherine@gmail.com>
2018-03-24 19:18:29 +0100

    version 2 Contract 2 24032018

--- src/io/github/oliviercailloux/y2018/twod_library/Contract/SearchData.java
+++ src/io/github/oliviercailloux/y2018/twod_library/Contract/SearchData.java
@@ -2,17 +2,18 @@ package io.github.oliviercailloux.y2018.twod_library.Contract;
 
 import java.util.Scanner;
 
-/**
- * Contract 2
+
+/** Contract 2
  * 
- * SearchData allows you to store data from a research. It stores the
- * information about: the book's title that has been searched,
- *  the book's author that has been searched
- *  and the book's publications dates that have been searched.
+ * SearchData allows you to store data from a research.
+ * It stores the information about:
+ * the book's title that has been searched
+ * the book's author that has been searched
+ * the book's publications dates that have been searched.
  * 
  * @author CatherineCAROU & daono
  * @version 2
- * @date 23/03/18
+ * @date 24/03/18
  */
 
 public class SearchData {
@@ -21,87 +22,105 @@ public class SearchData {
 	private String dataAuthors;
 	private int dataFirstDatePublication;
 	private int dataSecondDatePublication;
-	private Scanner scannerTitle;
-	private Scanner scannerAuthor;
-	private Scanner scannerDate1;
-	private Scanner scannerDate2;
-
-
-	public void setFirstDataDatePublication(int dataDatePublication) {
-		this.dataFirstDatePublication = dataDatePublication;
-
-	}
-
-	public void setSecondDataDatePublication(int dataDatePublication) {
-		this.dataSecondDatePublication = dataDatePublication;
-
-	}
 
 	public SearchData() {
 		this.dataTitles = "";
 		this.dataAuthors = "";
 		this.dataFirstDatePublication = 0;
 		this.dataSecondDatePublication = 0;
-
 	}
 
 	/**
 	 * searchTitle allows the user to search for a book's title
 	 */
-
-	public void searchTitle() {
+	public void searchTitle(){
 		System.out.println("What is the title of the book you are looking for ?");
-		scannerTitle = new Scanner(System.in);
-		String input = scannerTitle.nextLine();
-		this.dataTitles = input;
-System.out.println(input);
-		scannerTitle.close();
+		String input = TextIO.getlnString();
+		while (input.isEmpty()){
+			input = TextIO.getlnString();
+		}
+		this.setDataTitles(input);
+		//System.out.println(input);	test	 
 	}
 
 	/**
 	 * searchAuthor allows the user to search for a book's author
 	 */
-
-	public void searchAuthor() {
+	public void searchAuthor(){
 		System.out.println("What is the author of the book you are looking for ?");
-		scannerAuthor = new Scanner(System.in);
-		String input = scannerAuthor.nextLine();
-		this.dataTitles = input;
-
-		scannerAuthor.close();
+		String input = TextIO.getlnString();
+		while (input.isEmpty()){
+			input = TextIO.getlnString();
+		}
+		this.setDataAuthors(input);
+		//System.out.println(input);		test	
 	}
 
 	/**
-	 * searchDatePublication allows the user to search for a book's date of
-	 * publication
+	 * searchDatePublication allows the user to search for a book's date of publication
 	 */
 	public void searchDatePublication() {
 		System.out.println("Give the period of publication of the book you are looking for (two dates)");
-		
-		System.out.println("Give the first date");
-		
-		scannerDate1 = new Scanner(System.in);
-		int input1 = scannerDate1.nextInt();
-		
-		if (input1 <= 0) {
-			throw new NumberFormatException("Your date of publication must be strictly ypositive");
 
+		// FOR THE FIRST DATE
+		System.out.println("Give the first date");
+		int input1 = TextIO.getlnInt();
+		System.out.println((input1));
+		if (input1 <= 0) {
+			throw new NumberFormatException("Your date of publication must be strictly positive");
 		}	
 		this.setFirstDataDatePublication(input1);
 
-	
+		// FOR THE SECOND DATE
 		System.out.println("Give the second date");
-		scannerDate2 = new Scanner(System.in);
-		int input2 = scannerDate2.nextInt();
-
+		int input2 = TextIO.getlnInt();
+		System.out.println((input2));
 		if (input2 <= 0) {
 			throw new NumberFormatException("Your date of publication must be strictly positive");
-
-		}
-
+		}	
 		this.setSecondDataDatePublication(input2);
-		scannerDate1.close();
-		scannerDate2.close();
-		
+	}
+
+
+	public String getDataTitles() {
+		return dataTitles;
+	}
+
+	public void setDataTitles(String dataTitles) {
+		this.dataTitles = dataTitles;
+	}
+
+	public String getDataAuthors() {
+		return dataAuthors;
+	}
+
+	public void setDataAuthors(String dataAuthors) {
+		this.dataAuthors = dataAuthors;
+	}
+
+	public int getDataFirstDatePublication() {
+		return dataFirstDatePublication;
+	}
+
+	public void setDataFirstDatePublication(int dataFirstDatePublication) {
+		this.dataFirstDatePublication = dataFirstDatePublication;
+	}
+
+	public int getDataSecondDatePublication() {
+		return dataSecondDatePublication;
+	}
+
+	public void setDataSecondDatePublication(int dataSecondDatePublication) {
+		this.dataSecondDatePublication = dataSecondDatePublication;
+	}
+
+	public void setFirstDataDatePublication(int dataDatePublication) {
+		this.setDataFirstDatePublication(dataDatePublication);
+
+	}
+
+	public void setSecondDataDatePublication(int dataDatePublication) {
+		this.setDataSecondDatePublication(dataDatePublication);
+
 	}
 }
