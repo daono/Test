b504c79571dd198df6b23916bd50f9db27402543
CatherineCAROU <carou.catherine@gmail.com>
2018-03-29 17:18:38 +0200

    commit version du jeudi 29/03/2018

---
+++ src/io/github/oliviercailloux/y2018/twod_library/Contract/BookWriter.java
@@ -0,0 +1,73 @@
+package io.github.oliviercailloux.y2018.twod_library.Contract;
+
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+// CLASSES QUI PERMET D'ECRIRE DES LIVRERS AU FORMATS CSV
+public class BookWriter {
+	
+		//Delimiter used in CSV file
+		private static final String COMMA_DELIMITER = ",";
+		private static final String NEW_LINE_SEPARATOR = "\n";
+		
+		//CSV file header
+		private static final String FILE_HEADER = "title, author, datePublication";
+
+		//c.createline("romeo et juliet","shakespare","1400")
+		// writecsvfile(
+		public static void writeCsvFile(String fileName) {
+			
+			//Create new book objects
+			Livre l1 = new Livre("Hamlet","Shakespare",1400);
+				
+			//Create a new list of student objects
+			List books = new ArrayList();
+			books.add(l1);
+		
+			FileWriter fileWriter = null;
+					
+			try {
+				fileWriter = new FileWriter(fileName);
+
+				//Write the CSV file header
+				fileWriter.append(FILE_HEADER.toString());
+				
+				//Add a new line separator after the header
+				fileWriter.append(NEW_LINE_SEPARATOR);
+				
+				//Write a new student object list to the CSV file
+				for (Student student : students) {
+					fileWriter.append(String.valueOf(student.getId()));
+					fileWriter.append(COMMA_DELIMITER);
+					fileWriter.append(student.getFirstName());
+					fileWriter.append(COMMA_DELIMITER);
+					fileWriter.append(student.getLastName());
+					fileWriter.append(COMMA_DELIMITER);
+					fileWriter.append(student.getGender());
+					fileWriter.append(COMMA_DELIMITER);
+					fileWriter.append(String.valueOf(student.getAge()));
+					fileWriter.append(NEW_LINE_SEPARATOR);
+				}
+
+				
+				
+				System.out.println("CSV file was created successfully !!!");
+				
+			} catch (Exception e) {
+				System.out.println("Error in CsvFileWriter !!!");
+				e.printStackTrace();
+			} finally {
+				
+				try {
+					fileWriter.flush();
+					fileWriter.close();
+				} catch (IOException e) {
+					System.out.println("Error while flushing/closing fileWriter !!!");
+	                e.printStackTrace();
+				}
+				
+			}
+		}
+	}
+}
---
+++ src/io/github/oliviercailloux/y2018/twod_library/Contract/Main.java
@@ -0,0 +1,13 @@
+package io.github.oliviercailloux.y2018.twod_library.Contract;
+
+	public class Main {
+
+		public static void main(String[] args) {
+			SearchData s = new SearchData();
+			s.searchTitle();
+			s.searchAuthor();
+			//s.searchDatePublication();
+		}
+	}
+
+
