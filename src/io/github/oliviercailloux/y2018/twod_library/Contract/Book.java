118a8cf56697a637348da1ff599b8ebe4a4fd97f
julienneisha04 <julienneisha@yahoo.fr>
2018-04-07 08:48:24 +0200

    ajout commit

---
+++ src/io/github/oliviercailloux/y2018/twod_library/Contract/Book.java
@@ -0,0 +1,59 @@
+package io.github.oliviercailloux.y2018.twod_library.Contract;
+
+public class Book {
+
+
+	private String nbOfPages;
+	private String author;
+	private String publishingDate;
+	private String title;
+	private String genre;
+
+	public Book(String title, String author, String genre, String publishingDate, String nbOfPages2) {
+		this.setTitle(title);
+		this.setGenre(genre);
+		this.setAuthor(author);
+		this.setpublishingDate(publishingDate);
+		this.setNbOfPages(nbOfPages2);
+	}
+
+	public String getpublishingDate() {
+		return publishingDate;
+	}
+
+	public void setpublishingDate(String publishingDate) {
+		this.publishingDate = publishingDate;
+	}
+
+	public String getNbOfPages() {
+		return nbOfPages;
+	}
+
+	public void setNbOfPages(String nbOfPages2) {
+		this.nbOfPages = nbOfPages2;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+}
