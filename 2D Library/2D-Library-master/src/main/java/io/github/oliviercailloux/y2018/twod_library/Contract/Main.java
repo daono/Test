6ccbd63bca82170c8321793d48f61161ce70422b
daono <noemie.dao@hotmail.fr>
2018-04-07 12:41:13 +0200

    Test ajout derniere version du 6 avril

--- 2D Library/2D-Library-master/src/main/java/io/github/oliviercailloux/y2018/twod_library/Contract/Main.java
+++ 2D Library/2D-Library-master/src/main/java/io/github/oliviercailloux/y2018/twod_library/Contract/Main.java
@@ -9,15 +9,24 @@ package io.github.oliviercailloux.y2018.twod_library.Contract;
 //			s.searchDatePublication();
 			
 			Library l = new Library();
+			
+			// For File2
+			
 			Book b1 = new Book("Romeo et Juliette", "Shakespeare",1597);
-			Book b2 = new Book("harry potter a l'ecole des sorciers", "J.K Rowling",1997);
+			Book b2 = new Book("Harry Potter à l'école des sorciers", "J.K Rowling",1997);
 			l.getBooks().add(b1);
 			l.getBooks().add(b2);
+			
+			//test
 			// System.out.println(l.getBooks().get(0).getTitle());
-//			BookWriter bw= new BookWriter();
-			BookWriter.writeCsvFile(l,"test.csv");
+			
+			BookWriter.writeCsvFile(l,"src/io/github/oliviercailloux/y2018/twod_library/Resources/books-file2.csv");
 			
 			
+			// For Resource2
+			//by file in the classpath
+			
+			//by file in the main project
 		}
 	}
 
